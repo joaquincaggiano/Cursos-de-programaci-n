e5d33ee6a33cbad10aae14926286dc49eee06b12
joaquincaggiano <joaquincaggiano@gmail.com>
2022-02-21 13:34:36 -0300

    agregue todo devuelta de los cursos

---
+++ Udemy/Repositorio de Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/assigments.js
@@ -0,0 +1,97 @@
+/*const country = "Argentina";
+const continent = "Sudamérica";
+let population = 40;
+
+const isIsland = false;
+let language = "castellano";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+//console.log(population/2)
+population ++;
+//console.log(population)
+
+let finland = 60;
+//console.log (population > finland)
+let averageCountry = 33;
+//console.log (population < averageCountry)
+
+let description = country + " is in " + continent +", and its " + population +" million people speak "+language
+//console.log (description)*/
+
+//coding challenge 1
+/*let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 92;
+let johnHeight = 1.95;
+
+let markBMI = markMass / markHeight ** 2
+let johnBMI = johnMass / johnHeight ** 2
+
+//console.log(markBMI, johnBMI)
+
+let markHigherBMI = markBMI > johnBMI
+//console.log(markHigherBMI)*/
+
+//coding challenge 2
+
+/*if(markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's(${johnBMI})!`)
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's(${markBMI})!`)
+}*/
+
+//coding challenge 3
+
+/*const teamDolphins = (96 + 108 + 89) / 3;
+const teamKoalas = (88 + 91 + 110) / 3;
+console.log(teamDolphins, teamKoalas)
+
+if (teamDolphins > teamKoalas){
+    console.log("Team Dolphins is the winner")
+} else if (teamDolphins === teamKoalas) {
+    console.log("it`s a draw")
+} else {
+    console.log("Team Kolas is the winner")
+}
+
+//Bonus 1
+const teamDolphins = (97 + 112 + 101) / 3;
+const teamKoalas = (109 + 95 + 123) / 3;
+
+if (teamDolphins > teamKoalas && teamDolphins >= 100){
+    console.log("Team Dolphins is the winner")
+} else if (teamDolphins < teamKoalas && teamKoalas >= 100) {
+    console.log("Team Kolas is the winner")
+} else if (teamDolphins === teamKoalas && teamDolphins >= 100 && teamKoalas >= 100) {
+    console.log("It is a draw")
+} else {
+    console.log ("No one wins the trophy")
+}
+
+//Bonus 2
+const teamDolphins = (97 + 112 + 101) / 3;
+const teamKoalas = (109 + 95 + 106) / 3;
+
+if (teamDolphins > teamKoalas && teamDolphins >= 100){
+    console.log("Team Dolphins is the winner")
+} else if (teamDolphins === teamKoalas && (teamKoalas && teamDolphins) >= 100) {
+    console.log("it`s a draw")
+} else {
+    console.log("Team Kolas is the winner")
+}*/
+
+//coding challenge 4
+/*const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value was ${bill + tip}`)*/
+
+
+
+
+
+
