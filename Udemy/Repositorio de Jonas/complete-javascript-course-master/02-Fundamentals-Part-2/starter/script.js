c8cef3646528572125466f11b7c8fb3299429aec
joaquincaggiano <joaquincaggiano@gmail.com>
2022-02-23 20:00:53 -0300

    avanzando en el curso de JS

--- Udemy/Repositorio de Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Udemy/Repositorio de Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,67 @@
 "use strict" 
 
+//coding challenge 1
+
+/*const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3
+
+const averageDolphins = calcAverage(44, 23, 71)
+const averageKoalas = calcAverage(65, 54, 49)
+console.log (averageDolphins, averageKoalas)
+
+function checkWinner(avgDolphins, avgKoalas) {
+    if (avgDolphins >= avgKoalas * 2) {
+        console.log(`The dolphin´s team is the winner with a score of ${avgDolphins}`)
+    } else if (avgKoalas >= avgDolphins * 2) {
+        console.log(`The Koala´s team is the winner with a score of ${avgKoalas}`)
+    } else {
+        console.log(`No one win the competition`)
+    }
+}
+
+console.log(checkWinner(averageDolphins, averageKoalas))*/
+
+//coding challenge 2
+
+/*function calcTip (bill) {
+    if(bill >= 50 && bill <= 300) {
+        return bill * 0.15
+    } else {
+        return bill * 0.20
+    }
+}
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const total = [bills[0] + calcTip(bills[0]), bills[1] + calcTip(bills[1]), bills[2] + calcTip(bills[2])]
+console.log(bills,tips,total)*/
+
+//coding challenge 3
+/*const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi
+    }
+}
+
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / this.height ** 2
+        return this.bmi
+    }
+}
+
+mark.calcBMI()
+john.calcBMI()
+console.log(mark.bmi, john.bmi)
+
+console.log(`${mark.fullName}´s BMI (${mark.bmi}) is higher than ${john.fullName}´s (${john.bmi}))`)*/
+
+//coding challenge 4
 
 
