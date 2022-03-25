bbcdc18b1a54672fdcf7d8f719cdcf2242994512
joaquincaggiano <joaquincaggiano@gmail.com>
2022-03-25 14:26:25 -0300

    clase 44

---
+++ Digital house/Clases/Módulo 9 - Introducción a React/Clase 43 - Creando una aplicación con React/materiales-para-la-clase/my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import SideBar from './components/SideBar';
+import ContentWrapper from './components/ContentWrapper';
+
+function App() {
+  return (
+    <>
+      <div id="wrapper">
+        <SideBar />
+        <ContentWrapper />
+      </div>
+    </>
+  );
+}
+
+export default App;
+
