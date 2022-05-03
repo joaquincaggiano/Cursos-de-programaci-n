500eac293c774795664322155778cc799f4ee034
joaquincaggiano <joaquincaggiano@gmail.com>
2022-05-03 16:14:09 -0300

    javascript

--- Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -71,7 +71,150 @@ const restaurant = {
 // // spread operator
 // add(...x)//6
 
+// NULLISH OPERATOR
+// restaurant.numGuest = 0;
+// const guestCorrect = restaurant.numGuest ?? 10;
+// console.log(guestCorrect);// el valor da 0
+
+// LOGICAL ASSIGMENTS OPERATORS
+const rest1 = {
+  name: "Capri",
+  numGuests: 20
+}
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi"
+}
+
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+// // Operador de asignación de órdenes
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Operador de asignación nullish
+// Si el rest1 tuviera de numGuests = 0, las líneas anteriores darían de resultado 10 porque 0 es un valor falso
+// rest1.numGuests ??= 10;
+// rest2.numGuests ??= 10;
+
+// Operador de asignación AND
+// rest1.owner &&= "ANONYMOUS";
+// rest2.owner &&= "ANONYMOUS";
+
+// CODING CHALLENGE 1
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//   [
+//   'Neuer',
+//   'Pavard',
+//   'Martinez',
+//   'Alaba',
+//   'Davies',
+//   'Kimmich',
+//   'Goretzka',
+//   'Coman',
+//   'Muller',
+//   'Gnarby',
+//   'Lewandowski',
+//   ],
+//   [
+//   'Burki',
+//   'Schulz',
+//   'Hummels',
+//   'Akanji',
+//   'Hakimi',
+//   'Weigl',
+//   'Witsel',
+//   'Hazard',
+//   'Brandt',
+//   'Sancho',
+//   'Gotze',
+//   ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//   'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//   team1: 1.33,
+//   x: 3.25,
+//   team2: 6.5,
+//   },
+//   };
+
+//   // Task 1
+//   const [players1, players2] = game.players;
+
+//   // Task 2
+//   const [gk, ...fieldPlayers] = players1;
+
+//   // Task 3
+//   const allPlayers = [...players1, ...players2];
+
+//   // Task 4
+//   const players1Final = [...players1, 'Thiago', 'Coutinho','Perisic'];
+
+//   // Task 5
+//   const {odds: {team1, x: draw, team2}} = game;
+
+//   // Task 6
+//   const printGoals = function (...playersNames) {
+//     console.log(`${playersNames.length} goals were scored`);
+//   }
+
+//   printGoals(...game.scored);
+
+//   // Task 7
+//   team1 < team2 && console.log("Team 1 is more likely to win");
+//   team1 > team2 && console.log("Team 2 is more likely to win");
+  
+// FOR OF LOOP
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+// for (const item of menu) console.log(item);//Pizza Pasta Risotto Focaccia Bruschetta Garlic Bread Caprese Salad
+
+// for (const item of menu.entries()) {
+//   console.log(item); //[ 1, 'Pasta' ][ 2, 'Risotto' ][ 3, 'Focaccia' ][ 4, 'Bruschetta' ][ 5, 'Garlic Bread' ][ 6, 'Caprese Salad' ]
+// }
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);//1: Pizza 2: Pasta 3: Risotto 4: Focaccia 5: Bruschetta 6: Garlic Bread 7: Caprese Salad
+// }
 
 
+// OPTIONAL CHAINING
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open);
+// }
+// // with optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
 
+// const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+// for(const day of days) {
+//   const open = restaurant.openingHours[day]?.open ?? "closed";
+//   console.log(`On ${day}, we open at ${open}`);
+// }
 
+// console.log(restaurant.order?.(0,1) ?? "Method does not exit");
+
+// const users = [{name:"joaquin"}];
+// console.log(users[0]?.name ?? "User is empty");//joaquin
+
+// LOOPING OBJECTS
+// for (const day of Object.keys(restaurant.openingHours)){
+//   console.log(day);//thu, fri, sat
+// }
+
+// const values =Object.values(restaurant.openingHours);
+// console.log(values);//[{ open: 12, close: 22 },{ open: 11, close: 23 },{ open: 0, close: 24 }]
+
+const entries = Object.entries(restaurant.openingHours);
+// console.log(entries);
+for (const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+  //On thu we open at 12 and close at 22. On fri we open at 11 and close at 23. On sat we open at 0 and close at 24
+}
