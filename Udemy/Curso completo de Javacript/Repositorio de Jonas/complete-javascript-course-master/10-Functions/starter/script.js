4b98dd3f01ba16b54d525d629558614b28a55155
joaquincaggiano <joaquincaggiano@gmail.com>
2022-05-17 15:16:45 -0300

    modulo de array

--- Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -75,7 +75,7 @@
 
 // lufthansa.book(239, "Joaquín Caggiano");//Joaquín Caggiano booked a seat on Lufthansa flight LH239
 // lufthansa.book(639, "Alumine Llado");//Alumine Llado booked a seat on Lufthansa flight LH639
-// console.log(lufthansa);
+// // console.log(lufthansa);
 
 // const eurowings = {
 //     airline: "Eurowings",
@@ -101,3 +101,142 @@
 // book.call(eurowings, ...flightData);
 
 // THE BIND METHOD
+// const bookEW = book.bind(eurowings);
+// bookEW(666, "Dario Barassi");
+// // console.log(eurowings);//{ flight: 'EW666', name: 'Dario Barassi' }
+// // Podemos establecer parámetros antes
+// const bookEW666 = book.bind(eurowings, 666);
+// bookEW666("Goku");
+// // console.log(eurowings);//{ flight: 'EW666', name: 'Goku' }
+// const bookEWBatman = book.bind(eurowings, null, "Batman");
+// bookEWBatman(134);
+// console.log(eurowings);//{ flight: 'EWnull', name: 'Batman' }
+
+// with Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//     console.log(this);
+//     this.planes++
+//     console.log(this.planes);
+// }
+// document.querySelector(".buy").addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// partial application
+// const addTax = (rate, value) => value + value * rate;
+// const addVAT = addTax.bind(null, 0.23);
+// // addVat = value => value + value * 0.23
+
+// hacerlo retornando una function
+// const addTaxRate = function(rate){
+//     return function (value) {
+//         return value + value * rate
+//     }
+// };
+// const addVAT2 = addTaxRate(0.23);
+// console.log(addVAT2(100));//123
+// console.log(addVAT2(23));//28.29
+
+// CODING CHALLENGE 1
+// const poll = {
+//     question: "What is your favourite programming language?",
+//     options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+//     // This generates [0, 0, 0, 0]. More in the next section!
+//     answers: new Array(4).fill(0),
+//     registerNewAnswer() {
+//         const answer = Number(prompt(`${this.question} \n ${this.options.join("\n")} \n (Write option number)`));
+//         // console.log(answer);
+//         typeof answer === "number" && answer < this.answers.length && this.answers[answer]++;
+//         // console.log(this.answers);
+//         this.displayResults();
+//         this.displayResults("string")
+//     },
+//     displayResults(type = "array"){
+//         if(type === "array"){
+//             console.log(this.answers);
+//         } else if (type === "string") {
+//             console.log(`Poll results are ${this.answers.join(", ")}`);
+//         }
+//     }
+// };
+
+// document.querySelector(".poll").addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({answers: [5, 2, 3]}, "string");
+// poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, "string");
+
+// INMEDIATLY INVOKED FUNCTIONS EXPRESSIONS
+// (function(){
+//     console.log("This function will never run again");
+// })();
+// // con arrow function también funciona
+// (() => console.log("This function will ALSO never run again"))()
+// // private data
+// {
+//     const privateData = "Joaquín"
+// }
+// console.log(privateData);//ReferenceError: privateData is not defined
+
+// CLOSURES
+// const secureBooking = function () {
+//     let passengerCount = 0;
+
+//     return function () {
+//         passengerCount++;
+//         console.log(passengerCount);
+//     }
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// console.dir(booker);
+
+// example 1
+// let f;
+
+// const g = function () {
+//     const a = 23;
+//     f = function () {
+//         console.log(a * 2);
+//     } 
+// }
+
+// const h = function () {
+//     const b = 4;
+//     f = function () {
+//         console.log(b * 3);
+//     }
+// }
+
+// g();
+// f();
+// console.dir(f);//46 y en el scope, el closure es "a: 23"
+
+// h();
+// f();
+// console.dir(f);//12 y en el scope, el closure es "b: 4"
+
+// Example 2
+// const boardPassengers = function (n, wait) {
+//     const perGroup = n / 3;
+
+//     setTimeout(function () {
+//         console.log(`We are now boarding all ${n} passengers`);
+//         console.log(`There are 3 groups, each with ${perGroup} passengers`);
+//     }, wait * 1000);
+
+//     console.log(`Will start boarding in ${wait} seconds`);
+// }
+
+// boardPassengers(180, 3);
+
+// CODING CHALLENGE 2
+// (function () {
+//     const header = document.querySelector('h1');
+//     header.style.color = 'red';
+
+//     document.querySelector("body").addEventListener("click", () => {
+//         header.style.color = "blue"
+//     })
+// })();
--- Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Udemy/Curso completo de Javacript/Repositorio de Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -61,16 +61,104 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = "";//para vaciar todo lo que había antes y empezar de 0.
+
+    movements.forEach((mov, i) => {
+        const type = mov > 0 ? "deposit" : "withdrawal"
+        const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `
+        containerMovements.insertAdjacentHTML("afterbegin", html)
+    })
+}
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// SIMPLE ARRAYS METHODS
+// const arr = ["a", "b", "c", "d", "e"];
+// Slice
+// console.log(arr.slice(2));// [ 'c', 'd', 'e' ]
+// console.log(arr.slice(2,4));// [ 'c', 'd' ]
+// console.log(arr.slice(-2));// [ 'd', 'e' ]
+// console.log(arr.slice(1, -2));// [ 'b', 'c' ]
+// console.log(arr.slice());// estamos copiando el array original
+
+// Splice
+// console.log(arr.splice(2));// [ 'c', 'd', 'e' ]
+// console.log(arr);// [ 'a', 'b' ]
+
+// Reverse
+// console.log(arr.reverse());// [ 'e', 'd', 'c', 'b', 'a' ]
+// console.log(arr); // [ 'e', 'd', 'c', 'b', 'a' ]
+
+// Concat
+// const arr2 = ["f", "j", "h", "i", "j"];
+// const letters = arr.concat(arr2);
+// console.log(letters); // ['a', 'b', 'c', 'd','e', 'f', 'j', 'h','i', 'j']
+// console.log([...arr, ...arr2]);// ['a', 'b', 'c', 'd','e', 'f', 'j', 'h','i', 'j'] En este caso no mutamos ningun array
+
+// Join
+// console.log(arr.join(" - ")); // a - b - c - d - e
+
+// THE NEW AT METHOD - AVERIGUAR PORQUE VSC NO ME LO LEE
+// const arr = [23, 13, 45];
+// console.log(arr[0]);// 23 - Como se hacía antes
+// console.log(arr.at(0)); // 23 -Como se hace ahora
+// // Conseguir el último elemento
+// console.log(arr[arr.length -1]); //45
+// console.log(arr.slice(-1)[0]);// 45
+// console.log(arr.at(-1)); //45
+
+// LOOPING ARRAYS:FOREACH
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// // Con for of
+// for(const [i, movement] of movements.entries()){
+//     if (movement > 0) {
+//         console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//     } else {
+//         console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);// Math.abs devuelve el valor absoluto de un número
+//     }
+// }
+// // con forEach
+// movements.forEach((movement, i, arr) => {
+//     if (movement > 0) {
+//         console.log(`Movement ${i + 1}: You deposited ${movement}`);
+//     } else {
+//         console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);// Math.abs devuelve el valor absoluto de un número
+//     }
+// })
+
+// FOREACH for MAPS and SETS
+// // Map
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+// ]);
+
+// currencies.forEach(function(value, key, map){
+//     console.log(`${key}: ${value}`);
+// })
+// // Set
+// const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+// console.log(currenciesUnique);
+// currenciesUnique.forEach(function (value, _, map) {
+//     console.log(`${value}: ${value}`);
+// })
