fd3d3ef1bb83982993d5999908c158b291eaed9c
joaquincaggiano <joaquincaggiano@gmail.com>
2022-07-18 15:04:21 -0300

    DOM js

--- Udemy/Javacript/Repositorio de Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ Udemy/Javacript/Repositorio de Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,12 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  return btn.addEventListener('click', openModal);
+});
+
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +34,74 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+///////////////////////////////
+
+// // SELECTING ELEMENTS
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+// console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+// console.log(allButtons);
+
+// console.log(document.getElementsByClassName("btn"));
+
+// CREATING and INSERTING ELEMENTS
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  "We use cookied for improved funcionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// header.append(message);
+// header.prepend(message);
+// header.append(message.cloneNode(true));
+header.before(message);
+// header.after(message);
+
+// DELETING ELEMENTS
+document.querySelector(".btn--close-cookie").addEventListener("click", () => {
+  message.remove();
+  // Antes se hacía así para remover un elemento:
+  // message.parentElement.removeChild(message);
+})
+
+// STYLES
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+// console.log(getComputedStyle(message));//todos los estilos que tiene
+// console.log(getComputedStyle(message).color);//rgb(187, 187, 187)
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+
+// setProperty
+// document.documentElement.style.setProperty("--color-primary", "orangered");
+
+// // ATTRIBUTES
+const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);//Bankist logo
+// console.log(logo.src);//ubicación de la imagen
+// console.log(logo.className);//nav__logo
+// logo.alt = "Beautiful minimalist logo"
+// console.log(logo.alt);//Beautiful minimalist logo
+
+// // Non-standard
+// console.log(logo.getAttribute("designer"));//Joaquin
+// console.log(logo.getAttribute("src"));//img/logo.png
+// logo.setAttribute("company", "Bankist")
+
+// // Data attributes
+// console.log(logo.dataset.versionNumber);//3.0
+
+// // CLASSES
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+
