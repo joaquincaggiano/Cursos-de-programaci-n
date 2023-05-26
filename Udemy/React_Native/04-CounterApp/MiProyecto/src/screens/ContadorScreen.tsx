5341d6b75a4295ce4465d430ddd9f1c8140e0955
joaquincaggiano <joaquincaggiano@gmail.com>
2023-05-26 19:29:18 -0300

    counter screen

---
+++ Udemy/React_Native/04-CounterApp/MiProyecto/src/screens/ContadorScreen.tsx
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+
+export const ContadorScreen = () => {
+  const [counter, setCounter] = useState(0);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Contador: {counter}</Text>
+
+      <TouchableOpacity
+        style={styles.fabLocationRight}
+        onPress={() => setCounter(counter + 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.fabLocationLeft}
+        onPress={() => setCounter(counter - 1)}>
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+  },
+  fabLocationRight: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25,
+  },
+  fabLocationLeft: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
